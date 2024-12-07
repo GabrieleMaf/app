1f9d704955a18069255a046e2ff367459b4bad62
Gabriele Maferri <gabriele.maferri@it.experis.com>
2024-12-07 19:10:07 +0100

    WIP Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { HomeComponent } from './features/home/home.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contacts', component: ContactsComponent}
+];
