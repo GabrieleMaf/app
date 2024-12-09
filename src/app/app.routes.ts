55ad5b4643c5acffac111cb9fe368811ebc3ead8
Gabriele Maferri <gabriele.maferri@it.experis.com>
2024-12-09 10:27:57 +0100

    Adjust Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './features/about/about.component';
 import { HomeComponent } from './features/home/home.component';
-import { ContactsComponent } from './features/contacts/contacts.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'contacts', component: ContactsComponent}
+  {path: 'contacts', component: ProjectsComponent}
 ];
